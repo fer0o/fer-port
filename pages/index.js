348ec3c7cd7b122922ec21da71b87f854cbdb0e6
fer0o <fer0osennin@gmail.com>
2022-06-17 03:32:05 -0500

    agregado hero

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import { Hero } from '../components/Hero/Hero'
+
 export default function Home () {
   return (
     <div>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <Hero />
     </div>
   )
 }
