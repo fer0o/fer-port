4dc7109168a914d16bb3438d17f4f2fdcb77bc54
Fernando Ángel -Medellín Cuevas <fer0osennin@gmail.com>
2022-07-21 19:35:40 -0500

    about terminada

--- components/Info/Card.js
+++ components/Info/Card.js
@@ -2,23 +2,25 @@ import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-export const Card = ({ title, description, image ,link }) => {
+export const Card = ({ title, description, image ,link,linkGithub }) => {
   return (
     <div>
       {/* card con grid */}
 
-      <div className='   bg-base-100 shadow-xl border border-white lg:m-8 lg:p-2 p-8'>
-        <div className=' drop-shadow shadow-white  w-full h-48 lg:w-full lg:h-96 '>
+      <div className='   bg-base-100 shadow-xl border border-white lg:m-8 lg:p-2 p-8 h-full'>
+        <div className=' drop-shadow shadow-white  w-full h-48 lg:w-full lg:h-80 '>
           <Image layout='fill' src={image} />
         </div>
         <div className='card-body'>
-          <h2 className='card-title block text-center'>{title}</h2>
+          <h2 className='card-title block text-center font-bold text-2xl'>{title}</h2>
           <p>{description}</p>
-          <div className='card-actions justify-center'>
-            <button className='bg-blue-500 text-white text-lg p-4 m-4 w-auto h-auto'>
+          
+            <button className='bg-blue-500 border-2 border-black text-white rounded-md text-lg p-4 mt-8'>
               <Link href={link}>Abrir enlace</Link>
             </button>
-          </div>
+            <button className='bg-black text-white border-2 border-white rounded-md text-lg p-4 mt-8'>
+              <Link href={linkGithub}>Link Github</Link>
+            </button>
         </div>
       </div>
     </div>
--- pages/about.js
+++ pages/about.js
@@ -12,27 +12,35 @@ const about = () => {
       </div>
 
       {/* segundo grid */}
-      <div className='grid lg:grid-cols-3 grid-cols-1 lg:gap-4 gap-4 m-8'>
+      <div>
+      <h2 className='text-3xl font-bold text-white block text-center'>Ultimos Proyectos </h2>
+      <div className='grid lg:grid-cols-3 grid-cols-1 lg:gap-4 gap-4 m-4'>
         {/* card animestation */}
         <Card
           title='Anime Station'
-          description='Página web únicamente con HTML, CSS y JavaScript de la materia de Desarrollo Web en la escuela de Coderhouse.'
-          image='/aboutFotos/foto2.jpg'
+          description='Página web dedicada a la información de algunos post de páginas de anime, manga y videojuegos. para la maquetación esta fue hecha con HTML, CSS (css puro y bootstrap) y javascript, hecha para el curso de Desarrollo Web de Coderhouse'
+          image='/gif/animeStation.gif'
           link={'https://anime-station-zeta.vercel.app/'}
+          linkGithub={'https://github.com/fer0o/coderhouseDesarrolloWeb'}
         />
         <Card
-          title='Anime Station'
-          description='Página web únicamente con HTML, CSS y JavaScript de la materia de Desarrollo Web en la escuela de Coderhouse.'
-          image='/aboutFotos/foto2.jpg'
-          link={'https://anime-station-zeta.vercel.app/'}
+          title='Presion Arterial'
+          description='Aplicación web para la medición de presión arterial de una persona y llevar un registro, así mismo ver información, riesgos, recomendaciones. Para su desarrollo se usó HTML, CSS (CSS puro, bootstrap) librerías como sweetalert para los mensajes y Javascript para el registro que se guarde en memoria y alteraciones del DOM con eventos. Esta aplicacion fue hecha para la materia de Javascript de la escuela Coderhouse '
+          image='/gif/presionArterial.gif'
+          link={'https://presion-arterial.vercel.app/'}
+          linkGithub={'https://github.com/fer0o/Js_Coderhouse'}
         />
         <Card
-          title='Anime Station'
-          description='Página web únicamente con HTML, CSS y JavaScript de la materia de Desarrollo Web en la escuela de Coderhouse.'
-          image='/aboutFotos/foto2.jpg'
-          link={'https://anime-station-zeta.vercel.app/'}
+          title='Mundialito Shop'
+          description='Página web que tiene como finalidad la creación de una tienda virtual de jerseys de los equipos participantes del mundial de qatar 2022. Esta página está realizada con ReactJs, TailwindCSS, sweetalert y firebase. Este proyecto esta realizado para la materia de ReactJs de la escuela CoderHouse'
+          image='/gif/mundialitoShop.gif'
+          link={'https://mundialito.vercel.app/'}
+          linkGithub={'https://github.com/fer0o/cursoReactCoderhouse'}
         />
       </div>
+
+      </div>
+      
 
       <FooterComponent />
     </div>
